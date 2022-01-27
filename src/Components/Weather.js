2d9e34cd658a3639d049057028865dafded24569
Matthew <matthewhcbates@gmail.com>
2022-01-27 14:00:47 -0700

    weather app working

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -2,16 +2,16 @@ import React, { useState, useEffect } from "react";
 import { Button, Card, Container, Image } from "react-bootstrap";
 import ResortList from "./ResortList";
 function Weather({ weather }) {
-  console.log(weather)
+  console.log(weather);
 
   // icon={weather.weather[0].icon}
   // clouds={weather.clouds}
   // temperature={weather.main.temp}
   // feelsLike={w.main.feels_like}
   // sunset={w.sys.sunset}
-if (weather === null) {
-  return <h2>...Loading</h2>
-}
+  if (weather === null) {
+    return <h2>...Loading</h2>;
+  }
 
   return (
     <Container className={"d-flex align-items-center justify-content-center"}>
@@ -22,8 +22,18 @@ if (weather === null) {
           </Card.Title>
           <Card.Text>
             <div>
-              <p>Current Temp: {weather.main.temp} ℉</p>
-              <p>Cloud Cover: {weather.clouds.all}%</p>
+              <div>
+                <p>
+                  <b>Current Temp:</b> {weather.main.temp} ℉
+                </p>
+                <p>
+                  <b>Feels Like:</b> {weather.main.feels_like} ℉
+                </p>
+              </div>
+              <p>
+                {weather.weather[0].description.toUpperCase()}<br/>
+                <b>Cloud Cover:</b> {weather.clouds.all}%
+              </p>
             </div>
           </Card.Text>
         </Card.Body>
